56046135ff17a2443cc5a47c72dcc4635a3e1018
James-McCombe <161503322+James-McCombe@users.noreply.github.com>
2024-09-01 16:20:25 -0500

    Enabled some trace logging

--- LootMasterPro.lua
+++ LootMasterPro.lua
@@ -83,17 +83,6 @@ local function DebugPrint(level, message)
     end
 end
 
--- Define the debug flags
--- local isDebugMode = true -- Set to false to disable debug messages
-
--- Function to print debug messages
---[[local function DebugPrint(message)
-    if isDebugMode then
-        print("[DEBUG]: " .. message)
-    end
-end]]
-
-
 frame:RegisterEvent("CHAT_MSG_LOOT")
 frame:RegisterEvent("ITEM_COUNT_CHANGED")
 
@@ -161,10 +150,10 @@ local function printTable(tbl, indent)
     for k, v in pairs(tbl) do
         local formatting = string.rep("  ", indent) .. k .. ": "
         if type(v) == "table" then
-            print(formatting)
+            DebugPrint(LogLevel.Trace, formatting)
             printTable(v, indent + 1)
         else
-            print(formatting .. tostring(v))
+            DebugPrint(LogLevel.Trace, formatting .. tostring(v))
         end
     end
 end
@@ -213,22 +202,16 @@ frame:SetScript("OnEvent", function(self, event, ...)
             fallbackTimer = wait(2, fallbackCheck)
         end
 
-        --[[if #pendingItemsQueue > 0 and not fallbackTimer then
-            fallbackTimer = wait(2, fallbackCheck)
-            DebugPrint(LogLevel.Debug, "Fallback timer set")
-        end]]
-
-
     elseif event == "ITEM_COUNT_CHANGED" then
         local itemNumber = tostring(...)
-        DebugPrint(LogLevel.Info, "ITEM_COUNT_CHANGED for item number: " .. itemNumber)
-        -- DebugPrint(LogLevel.Debug, "Type of itemNumber: " .. type(itemNumber))
-        -- DebugPrint(LogLevel.Debug, "Pending item data: " .. tostring(pendingItemData[itemNumber]))
-        -- DebugPrint(LogLevel.Debug, "Current pendingItemData contents:")
-        -- printTable(pendingItemData)
-        --[[for k, v in pairs(pendingItemData) do
-            DebugPrint(LogLevel.Debug, "Key: " .. k .. ", Type: " .. type(k))
-        end]]
+        DebugPrint(LogLevel.Info, "ITEM_COUNT_CHANGED for item number: [ " .. itemNumber .. " ]")
+        DebugPrint(LogLevel.Trace, "Type of itemNumber: " .. type(itemNumber))
+        DebugPrint(LogLevel.Trace, "Pending item data: " .. tostring(pendingItemData[itemNumber]))
+        DebugPrint(LogLevel.Trace, "Current pendingItemData contents:")
+        printTable(pendingItemData)
+        for k, v in pairs(pendingItemData) do
+            DebugPrint(LogLevel.Trace, "Key: " .. k .. ", Type: " .. type(k))
+        end
         if pendingItemData[itemNumber] then
             DebugPrint(LogLevel.Info, "Handled ITEM_COUNT_CHANGED for item number: [ " .. itemNumber .. " ]")
             local WowApiItemCountBag = GetItemCount(itemNumber) or 0
@@ -289,77 +272,3 @@ frame:SetScript("OnEvent", function(self, event, ...)
         end
     end
 end)
-
-
-
-
---[[
--- Add the filter function to the chat frame
-ChatFrame_AddMessageEventFilter("CHAT_MSG_LOOT", FilterLootMessage)
-
-frame:SetScript("OnEvent", function(self, event, message)
-    if event == "CHAT_MSG_LOOT" and string.find(message, "You receive ") then
-	
-		-- Get Latency Calculated Delay
-		local decWaitDelay = GetDecWaitDelay()
-
-        -- Extract the item link from the message
-        local itemLink = message:match("|Hitem:.-|h.-|h")
-        local intItemCount = tonumber(message:match("x(%d+)")) or 1
-        local itemString = TSM_API.ToItemString(itemLink)
-        local itemNumber = itemString:gsub("i:", "")
-        local intNumPlayer, intNumAlts, intNumAuctions, intNumAltAuctions = TSM_API.GetPlayerTotals(itemString)
-        local intNumBags = TSM_API.GetBagQuantity(itemString) or 0
-        local intMail = TSM_API.GetMailQuantity(itemString) or 0
-        local intGetGuildBank = TSM_API.GetGuildQuantity(itemString) or 0
-        local intWarBankTotal = TSM_API.GetWarbankQuantity(itemString) or 0
-        local intNumInBagsLocal = GetItemCount(itemNumber) or 0
-		
-		local intPricedbMarketValue = intItemCount * (TSM_API.GetCustomPriceValue("dbMarket", itemString) or 0)
-		local strPricedbMarketString = TSM_API.FormatMoneyString(intPricedbMarketValue)
-		local intPriceVendorSellValue = intItemCount * (TSM_API.GetCustomPriceValue("VendorSell", itemString) or 0)
-		local strPriceVendorSellValue = TSM_API.FormatMoneyString(intPriceVendorSellValue)
-
-		DebugPrint("WOW_API: Item Number: " .. itemNumber)
-		DebugPrint("WOW_API: Items Collected: " .. intItemCount)
-		local WowApiItemCountBag = GetItemCount(itemNumber) or 0
-		DebugPrint("WOW_API: Items in Bags: " .. WowApiItemCountBag)
-		local WowApiItemCountBank = (GetItemCount(itemNumber, true) or 0) - WowApiItemCountBag
-		DebugPrint("WOW_API: Items in Bank: " .. WowApiItemCountBank)
-		local WowApiItemCountReagentBank = (GetItemCount(itemNumber, false, false, true) or 0) - WowApiItemCountBag
-		DebugPrint("WOW_API: Items in Reagent Bank: " .. WowApiItemCountReagentBank)
-
-		local TSMApiItemCountBag = TSM_API.GetBagQuantity(itemString) or 0
-		DebugPrint("TSM_API: Items in Bags: " .. TSMApiItemCountBag)
-		local TSMApiItemCountBank = TSM_API.GetBankQuantity(itemString) or 0
-		DebugPrint("TSM_API: Items in Bank: " .. TSMApiItemCountBank)
-		local TSMApiItemCountReagentBank = TSM_API.GetReagentBankQuantity(itemString) or 0
-		DebugPrint("TSM_API: Items in Reagent Bank: " .. TSMApiItemCountReagentBank)
-		
-
-
-        -- Add a message before the delay
-        if (intNumBags + intItemCount) ~= intNumInBagsLocal then
-            wait(decWaitDelay, function()
-				local intNumPlayer, intNumAlts, intNumAuctions, intNumAltAuctions = TSM_API.GetPlayerTotals(itemString)
-				local intNumBags = TSM_API.GetBagQuantity(itemString) or 0
-				local intNumInBagsLocal = GetItemCount(itemNumber) or 0
-
-				if (intNumBags + intItemCount) ~= intNumInBagsLocal then
-					wait(decWaitDelay, function()
-						local intNumInBagsLocal = GetItemCount(itemNumber) or 0
-						local intNumPlayer, intNumAlts, intNumAuctions, intNumAltAuctions = TSM_API.GetPlayerTotals(itemString)
-						DEFAULT_CHAT_FRAME:AddMessage("|cff009900" .. message .. "|cffffffff [Tot: " .. intNumPlayer .. ", Bag: " .. intNumInBagsLocal ..", Alt: " .. intNumAlts .. ", AH: " .. intNumAuctions + intNumAltAuctions .. ", WB: " .. intWarBankTotal .. "] [AH: " .. strPricedbMarketString .. " V: " .. strPriceVendorSellValue .. "]")
-					end)
-				else
-					DEFAULT_CHAT_FRAME:AddMessage("|cff009900" .. message .. "|cffffffff [Tot: " .. intNumPlayer .. ", Bag: " .. intNumInBagsLocal ..", Alt: " .. intNumAlts .. ", AH: " .. intNumAuctions + intNumAltAuctions .. ", WB: " .. intWarBankTotal .. "] [AH: " .. strPricedbMarketString .. " V: " .. strPriceVendorSellValue .. "]")
-				end
-
-            end)
-        else
-            -- If no delay needed, print the custom message immediately
-			DEFAULT_CHAT_FRAME:AddMessage("|cff009900" .. message .. "|cffffffff [Tot: " .. intNumPlayer .. ", Bag: " .. intNumInBagsLocal ..", Alt: " .. intNumAlts .. ", AH: " .. intNumAuctions + intNumAltAuctions .. ", WB: " .. intWarBankTotal .. "] [AH: " .. strPricedbMarketString .. " V: " .. strPriceVendorSellValue .. "]")
-        end
-    end
-end)
-]]
